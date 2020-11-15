866caa0f2d70eff300dcb2d796a55c03e80bbdbb
YiJie (Jackie) Zhu <64090163+jckpokimain@users.noreply.github.com>
2020-11-14 22:20:04 -0500

    form v1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import "./Form.css";
 
 export default function Form() {
     return (
-        <div>
-            Form
+        <div class="user-form">
+            <h1>Info Below</h1>
+            <div class="txtb">
+                <label>Full Name</label>
+                <input type="text" name="" value="" placeholder="Enter Your Name"></input>
+            </div>
+
+            <div class="txtb">
+                <label>Email</label>
+                <input type="email" name="" value="" placeholder="Enter Your Name"></input>
+            </div>
+
+            <div class="txtb">
+                <label>Future University</label>
+                <input type="text" name="" value="" placeholder="Enter Your Name"></input>
+            </div>
+
+            <div class="txtb">
+                <label>Program</label>
+                <input type="text" name="" value="" placeholder="Enter Your Name"></input>
+            </div>
+            <a href="#link" class="btn">Submit</a>
+            
         </div>
     )
 }
