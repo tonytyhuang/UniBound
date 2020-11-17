4d1711d4a03815683bb39c1c76f106cb312d3a69
YiJie (Jackie) Zhu <64090163+jckpokimain@users.noreply.github.com>
2020-11-16 22:44:11 -0500

    added hook to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,51 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 import "./Form.css";
 
 export default function Form() {
+
+        
+    const history = useHistory()
+    const {register, handleSubmit } = useForm()
+
+    const onSubmit = (values) => {
+        console.log(values);
+    }
+
     return (
+   
         <div class="user-form">
             <h1>Info Below</h1>
-            <div class="txtb">
-                <label>Full Name</label>
-                <input type="text" name="" value="" placeholder="Enter Your Name"></input>
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div class="txtb">
+                    <label>Full Name</label>
+                    <input type="text" name="name" placeholder="Enter Your Name" ref={register}/>
+                </div>
 
-            <div class="txtb">
-                <label>Email</label>
-                <input type="email" name="" value="" placeholder="Enter Your Name"></input>
-            </div>
+                <div class="txtb">
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="Enter Your Name" ref={register}/>
+                </div>
 
-            <div class="txtb">
-                <label>Future University</label>
-                <input type="text" name="" value="" placeholder="Enter Your Name"></input>
-            </div>
+                <div class="txtb">
+                    <label>University</label>
+                    <input type="text" name="university" placeholder="Enter Your Name" ref={register}/>
+                </div>
 
-            <div class="txtb">
-                <label>Program</label>
-                <input type="text" name="" value="" placeholder="Enter Your Name"></input>
-            </div>
-            <a href="#link" class="btnn">Submit</a>
+                <div class="txtb">
+                    <label>Program</label>
+                    <input type="text" name="program" placeholder="Enter Your Name" ref={register}/>
+                </div>
+
+                <div class="txtb">
+                    <label>Message</label>
+                    <textarea> </textarea>
+
+                </div>
+                <input type="submit" class = "btnn"/>
+            </form>
+
             
         </div>
     )
