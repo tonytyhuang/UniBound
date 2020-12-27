975086a5955664298f415df562f45ffef06239f0
Tony Huang <t.tonyhuang@gmail.com>
2020-12-26 22:18:34 -0500

    add user registration connection to mongo

---
+++ backend/routes/userRouter.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+
+
+router.post("/register", async (req, res) => {
+    try {
+      let { email, password, passwordCheck, displayName } = req.body;
+  
+      // validate
+  
+      if (!email || !password || !passwordCheck)
+        return res.status(400).json({ msg: "Not all fields have been entered." });
+      if (password.length < 5)
+        return res
+          .status(400)
+          .json({ msg: "The password needs to be at least 5 characters long." });
+      if (password !== passwordCheck)
+        return res
+          .status(400)
+          .json({ msg: "Enter the same password twice for verification." });
+  
+      const existingUser = await User.findOne({ email: email });
+      if (existingUser)
+        return res
+          .status(400)
+          .json({ msg: "An account with this email already exists." });
+  
+      if (!displayName) displayName = email;
+  
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+  
+      const newUser = new User({
+        email,
+        password: passwordHash,
+        displayName,
+      });
+      const savedUser = await newUser.save();
+      res.json(savedUser);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 5000;
 
+// setup for express
+
+const app = express();
+
+// choose between local and 
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+})
+
+// setup mongoose
+
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateindex: true});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateindex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+// set up routes
+
 const formsRouter = require('./routes/forms');
 app.use('/forms', formsRouter);
-
-app.listen(port, () => {
-    console.log('Server is running on port: ' + port);
-})
+const userRouter = require('./routes/userRouter')
+app.use('/about', userRouter);
